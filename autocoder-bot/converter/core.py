6b013b167e40430249ffdaf2d196bc64831a3a91
Zezva Okhanashvili <84275113+Zeizvant@users.noreply.github.com>
2026-01-18 14:54:17 +0400

    Delete autocoder-bot directory

--- autocoder-bot/converter/core.py
+++
@@ -1,7 +0,0 @@
-class MarkdownConverter:
-    def __init__(self):
-        pass
-
-    def convert(self, markdown_text):
-        # Convert markdown text to HTML
-        return markdown.markdown(markdown_text)
